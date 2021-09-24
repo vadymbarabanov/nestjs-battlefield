4f80ddc05342e05a08b4192525962e09ed8e523a
Vadym Barabanov <introvert.png@gmail.com>
2021-09-24 23:39:32 +0300

    Add env config setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,27 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UsersModule } from './users/users.module'
+import { ConfigModule } from '@nestjs/config'
 
-@Module({ controllers: [AppController], providers: [AppService] })
+@Module({
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+})
 export class AppModule {}
