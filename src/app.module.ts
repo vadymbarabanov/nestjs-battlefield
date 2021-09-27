6d12d2ce01da2c7cb79dbc0215b2b0c1ec0c63ad
Vadym Barabanov <introvert.png@gmail.com>
2021-09-27 22:25:28 +0300

    Add user model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './users/users.model';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -18,7 +19,7 @@ import { UsersModule } from './users/users.module';
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
       autoLoadModels: true,
-      models: [],
+      models: [User],
     }),
     UsersModule,
   ],
