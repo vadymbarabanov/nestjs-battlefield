e77bc3d424517b8eeca0cef51d809327d2bf6b61
Vadym Barabanov <introvert.png@gmail.com>
2021-09-27 22:30:48 +0300

    Add user service & dto

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { User } from './users.model';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User) private userRepository: typeof User) {}
+
+  async createUser(dto: CreateUserDTO) {
+    return await this.userRepository.create(dto);
+  }
+
+  async getAllUsers() {
+    return await this.userRepository.findAll();
+  }
+}
