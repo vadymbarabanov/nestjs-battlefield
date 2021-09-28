8616c68adc695c4a8518c800fac3d3018cd6b485
Vadym Barabanov <introvert.png@gmail.com>
2021-09-28 22:50:22 +0300

    Add swagger documetation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs practice')
+    .setDescription('Documentation REST API')
+    .setVersion('1.0.0')
+    .addTag('vadymbarabanov')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
   await app.listen(PORT, () =>
     console.log(`🚀 Server started on port http://locahost:${PORT}`),
   );
