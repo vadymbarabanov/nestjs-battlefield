7128116f42405cf77167d55cf5767785a95b47e4
Vadym Barabanov <introvert.png@gmail.com>
2021-09-30 23:21:50 +0300

    Add roles to users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,10 +5,12 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './users.model';
 import { Role } from 'src/roles/roles.model';
 import { UserRoles } from 'src/roles/user-roles.model';
+import { RolesModule } from 'src/roles/roles.module';
 
 @Module({
   providers: [UsersService],
   controllers: [UsersController],
-  imports: [SequelizeModule.forFeature([User, Role, UserRoles])],
+  imports: [SequelizeModule.forFeature([User, Role, UserRoles]), RolesModule],
+  exports: [UsersService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { RolesService } from 'src/roles/roles.service';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { User } from './users.model';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User) private userRepository: typeof User) {}
+  constructor(
+    @InjectModel(User) private userRepository: typeof User,
+    private roleService: RolesService,
+  ) {}
 
   async createUser(dto: CreateUserDTO) {
-    return this.userRepository.create(dto);
+    const user = await this.userRepository.create(dto);
+    const role = await this.roleService.getRoleByValue('USER');
+    await user.$set('roles', [role.id]);
+    user.roles = [role];
+    return user;
   }
 
   async getAllUsers() {
-    return this.userRepository.findAll();
+    return this.userRepository.findAll({ include: { all: true } });
+  }
+
+  async getByEmail(email: string) {
+    return this.userRepository.findOne({
+      where: { email },
+      include: { all: true },
+    });
   }
 }
