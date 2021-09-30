ba6e318a2e57d5a46dc1a7407d56034d473022a0
Vadym Barabanov <introvert.png@gmail.com>
2021-09-30 23:22:35 +0300

    Add auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateUserDTO } from 'src/users/dto/create-user.dto';
+import { AuthService } from './auth.service';
+
+@ApiTags('Authorization')
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  login(@Body() userDTO: CreateUserDTO) {
+    return this.authService.login(userDTO);
+  }
+
+  @Post('register')
+  register(@Body() userDTO: CreateUserDTO) {
+    return this.authService.register(userDTO);
+  }
+}
