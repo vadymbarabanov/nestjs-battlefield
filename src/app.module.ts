70d0a2616d6334976ce0cbac60d3cd2bfe1f4d4a
Vadym Barabanov <introvert.png@gmail.com>
2021-10-03 20:57:49 +0300

    Add post model

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { Role } from './roles/roles.model';
 import { UserRoles } from './roles/user-roles.model';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
+import { Post } from './posts/posts.model';
 
 @Module({
   controllers: [],
@@ -24,7 +25,7 @@ import { PostsModule } from './posts/posts.module';
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
       autoLoadModels: true,
-      models: [User, Role, UserRoles],
+      models: [User, Role, UserRoles, Post],
     }),
     UsersModule,
     RolesModule,
---
+++ src/posts/posts.model.ts
@@ -0,0 +1,55 @@
+import {
+  Column,
+  DataType,
+  Model,
+  Table,
+  BelongsToMany,
+  BelongsTo,
+  ForeignKey,
+} from 'sequelize-typescript';
+import { User } from 'src/users/users.model';
+
+interface PostCreationAttrs {
+  title: string;
+  content: string;
+  image: string;
+}
+
+@Table({ tableName: 'posts' })
+export class Post extends Model<Post, PostCreationAttrs> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  title: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  content: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  image: string;
+
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  userId: number;
+
+  @BelongsTo(() => User)
+  author: User;
+}
